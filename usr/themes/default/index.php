4ea3ce00e953a958e55edd91f61e6b80dd2fa0fc
he.wenyao <he.wenyao@qq.com>
2021-12-03 18:00:52 +0800

    更改样式

--- usr/themes/default/index.php
+++ usr/themes/default/index.php
@@ -26,6 +26,7 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
                 <li itemprop="author" itemscope itemtype="http://schema.org/Person"><?php _e('作者: '); ?><a itemprop="name" href="<?php $this->author->permalink(); ?>" rel="author"><?php $this->author(); ?></a></li>
                 <li><?php _e('时间: '); ?><time datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date(); ?></time></li>
                 <li><?php _e('分类: '); ?><?php $this->category(','); ?></li>
+                <li itemprop="keywords" class="tags"><?php _e('标签: '); ?><?php $this->tags(', ', true, 'none'); ?></li>
                 <!--<li itemprop="interactionCount"><a itemprop="discussionUrl" href="--><?php //$this->permalink() ?><!--#comments">--><?php //$this->commentsNum('评论', '1 条评论', '%d 条评论'); ?><!--</a></li>-->
             </ul>
         </article>
