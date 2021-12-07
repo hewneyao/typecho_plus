be081c31122443100c6639c456505e26e4d2036d
he.wenyao <he.wenyao@qq.com>
2021-12-07 18:54:36 +0800

    修改样式

--- usr/themes/default/header.php
+++ usr/themes/default/header.php
@@ -17,7 +17,8 @@
     <link rel="stylesheet" href="//cdnjscn.b0.upaiyun.com/libs/normalize/2.1.3/normalize.min.css">
     <link rel="stylesheet" href="<?php $this->options->themeUrl('grid.css'); ?>">
     <link rel="stylesheet" href="<?php $this->options->themeUrl('style.css'); ?>">
-
+    <link rel="stylesheet" href="<?php $this->options->themeUrl('./css/header.css'); ?>">
+    <link rel="stylesheet" href="<?php $this->options->themeUrl('./css/sidebar.css'); ?>">
     <!--[if lt IE 9]>
     <script src="//cdnjscn.b0.upaiyun.com/libs/html5shiv/r29/html5.min.js"></script>
     <script src="//cdnjscn.b0.upaiyun.com/libs/respond.js/1.3.0/respond.min.js"></script>
@@ -39,41 +40,52 @@
     <div class="browsehappy" role="dialog"><?php _e('当前网页 <strong>不支持</strong> 你正在使用的浏览器. 为了正常的访问, 请 <a href="http://browsehappy.com/">升级你的浏览器</a>'); ?>.</div>
 <![endif]-->
 
+<!--网站头部-->
 <header id="header" class="clearfix">
     <div class="container">
-        <div class="row">
-            <div class="site-name col-mb-12 col-9">
+        <div class="row" style="display: flex;align-items: center">
+<!--            <div class="site-name col-mb-12 col-2">-->
+            <div class="site-name col-2">
                 <?php if ($this->options->logoUrl): ?>
                     <a id="logo" href="<?php $this->options->siteUrl(); ?>">
                         <img src="<?php $this->options->logoUrl() ?>" alt="<?php $this->options->title() ?>"/>
                     </a>
                 <?php else: ?>
                     <a id="logo" href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title() ?></a>
-                    <p class="description"><?php $this->options->description() ?></p>
+                    <!--<p class="description"><?php /*$this->options->description() */?></p>-->
                 <?php endif; ?>
             </div>
-            <div class="site-search col-3 kit-hidden-tb">
+
+
+            <!--搜索框-->
+            <div class="site-search col-push-6 col-1 kit-hidden-tb">
                 <form id="search" method="post" action="<?php $this->options->siteUrl(); ?>" role="search">
                     <label for="s" class="sr-only"><?php _e('搜索关键字'); ?></label>
                     <input type="text" id="s" name="s" class="text" placeholder="<?php _e('输入关键字搜索'); ?>"/>
                     <button type="submit" class="submit"><?php _e('搜索'); ?></button>
                 </form>
             </div>
-            <div class="col-mb-12">
+            <!--搜索框 end-->
+            <!--导航栏-->
+            <div class="col-mb-12 col-mb-12 col-push-5 col-3" >
                 <nav id="nav-menu" class="clearfix" role="navigation">
                     <a<?php if ($this->is('index')): ?> class="current"<?php endif; ?>
                             href="<?php $this->options->siteUrl(); ?>"><?php _e('首页'); ?></a>
                     <?php $this->widget('Widget_Contents_Page_List')->to($pages); ?>
                     <?php while ($pages->next()): ?>
-                        <a<?php if ($this->is('page', $pages->slug)): ?> class="current"<?php endif; ?>
+                        <a      <?php if ($this->is('page', $pages->slug)): ?> class="current"<?php endif; ?>
                                 href="<?php $pages->permalink(); ?>"
                                 title="<?php $pages->title(); ?>"><?php $pages->title(); ?></a>
                     <?php endwhile; ?>
                 </nav>
             </div>
+            <!--导航栏-->
+
         </div><!-- end .row -->
     </div>
 </header><!-- end #header -->
+<!--网站头部-->
+
 <div id="body">
     <div class="container">
         <div class="row">
--- usr/themes/default/page.php
+++ usr/themes/default/page.php
@@ -1,15 +1,16 @@
+<!--ÐÂ½¨Ò³Ãæ-->
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <?php $this->need('header.php'); ?>
 
-<div class="col-mb-12 col-8" id="main" role="main">
+<div class="col-mb-12 col-push-1 col-10" id="main" role="main">
     <article class="post" itemscope itemtype="http://schema.org/BlogPosting">
         <h1 class="post-title" itemprop="name headline"><a itemprop="url" href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h1>
         <div class="post-content" itemprop="articleBody">
             <?php $this->content(); ?>
         </div>
     </article>
-    <?php $this->need('comments.php'); ?>
+<!--    --><?php //$this->need('comments.php'); ?>
 </div><!-- end #main-->
 
-<?php $this->need('sidebar.php'); ?>
+<?php //$this->need('sidebar.php'); ?>
 <?php $this->need('footer.php'); ?>
